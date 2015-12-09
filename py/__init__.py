94c58276ea67eef7bb065eec25aa8b1f4cf3b289
Liu,Xiaolin <liuxiaolin6@cmbc.com.cn>
2015-12-09 17:32:19 +0800

    Add timed_scope

--- py/__init__.py
+++ py/__init__.py
@@ -1,5 +1,8 @@
 ##-*-coding: utf-8;-*-##
+import contextlib
 import functools
+import sys
+import time
 
 
 def catch_type_error_in_op(special):
@@ -21,3 +24,18 @@ def coroutine(func):
         return gen
 
     return wrapper
+
+
+@contextlib.contextmanager
+def timed_scope(label=None):
+    t0 = time.time()
+    prefix = u'timed scope [{}]'.format('' if label is None else label)
+    print >>sys.stderr, prefix, u'started'
+    try:
+        yield
+    except Exception:
+        raise
+    finally:
+        t1 = time.time()
+        print >>sys.stderr, prefix, u'finished'
+        print >>sys.stderr, prefix, t1-t0, 'seconds used'
